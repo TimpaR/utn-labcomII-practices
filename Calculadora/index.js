8f4d4e115d431e505ff0b156463290220b93bc32
Renzo Timpanaro <renzotimpa11@gmail.com>
2023-05-16 11:22:24 -0300

    16/5/2023

---
+++ Calculadora/index.js
@@ -0,0 +1,42 @@
+function validacion() {
+    var user = document.getElementById("usuario").value;
+    var domi = document.getElementById("domicilio").value;
+    var cantidad = document.getElementById("cantidadKwh").value;
+    if (user == 1 || domi == 1 || cantidad <= 0) {
+        alert("Complete todos los campos")
+    } else {
+       return costo()
+    }
+}
+
+function costo() {
+    var a;
+    var b;
+    const servicio = 102;
+    var user = document.getElementById("usuario").value;
+    var domi = document.getElementById("domicilio").value;
+    var cantidad = document.getElementById("cantidadKwh").value;
+    if (user == "residencial") {
+        a = 0.21;
+    } else {
+        a = 0.27;
+    }
+
+    switch (domi) {
+        case "centro":
+            b = 5.80;
+            break;
+        case "sur":
+            b = 5.40;
+            break
+        case "oeste":
+            b = 5.35;
+            break;
+        case "norte":
+            b = 5.60;
+            break;
+    }
+
+    var total = Number((servicio + cantidad * b * (1 + a)).toFixed(2));
+    alert("El total es $" + total)
+}
